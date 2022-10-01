4eb748a490c17d38ed6c71baf219a99bbd0816d2
drsimplegraffiti <abayomiogunnusi@gmail.com>
2022-10-01 22:47:52 +0100

    Added url encoded middleware

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const port = process.env.PORT || 4555;
 
